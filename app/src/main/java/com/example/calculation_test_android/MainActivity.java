f54ebeb5bb0d4996f09a6523dd53da5feb7c7b87
yang <2358109030@qq.com>
2019-11-08 17:00:57 +0800

    解决上次调试问题，back能正常使用，并且页面添加一个返回键

--- app/src/main/java/com/example/calculation_test_android/MainActivity.java
+++ app/src/main/java/com/example/calculation_test_android/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.calculation_test_android;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controller = Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this,controller);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        if (controller.getCurrentDestination().getId() == R.id.questionFragment){
+            //判断fragment
+            //设置弹窗
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getString(R.string.quit_dialog_title));
+            builder.setPositiveButton(R.string.dialog_positive_message, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    controller.navigateUp();
+                }
+            });
+            builder.setNegativeButton(R.string.dialog_negative_message, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }else if(controller.getCurrentDestination().getId() == R.id.titleFragment) {
+            finish();//结束当前activity
+        }else{
+            controller.navigate(R.id.titleFragment);
+        }
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {//拦截back建
+        onSupportNavigateUp();
+    }
+
 }
